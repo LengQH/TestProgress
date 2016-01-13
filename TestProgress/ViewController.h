63331c64ddac7c595481fd73c1eb06f7489786c6
LengQiuHui <1205632644@qq.com>
2016-01-13 20:21:11 +0800

    Initial Commit

---
+++ TestProgress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProgress
+//
+//  Created by ma c on 16/1/13.
+//  Copyright © 2016年 gdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestProgress/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestProgress
+//
+//  Created by ma c on 16/1/13.
+//  Copyright © 2016年 gdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
